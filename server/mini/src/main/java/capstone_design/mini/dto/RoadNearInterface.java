52834b5302e8834cb73906828bb869e5c3fe4576
Sion Noh <shtldhs123@ajou.ac.kr>
2022-03-29 17:37:16 +0900

    인접 road api - 위경도 추가

--- server/mini/src/main/java/capstone_design/mini/dto/RoadNearInterface.java
+++ server/mini/src/main/java/capstone_design/mini/dto/RoadNearInterface.java
@@ -3,6 +3,17 @@ package capstone_design.mini.dto;
 public interface RoadNearInterface {
 
     double getD();
+
     Long getId();
+
     String getRoad_name();
+
+    double getStart_lat();
+
+    double getStart_lng();
+
+    double getEnd_lat();
+
+    double getEnd_lng();
+
 }
